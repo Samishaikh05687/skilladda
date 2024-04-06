c1d0e17009ce79a61012a718214f568f11d37ff1
sami shaikh <samishaikh05687@gmail.com>
2024-04-07 02:09:19 +0530

    commit7

---
+++ app/seller/layout.tsx
@@ -0,0 +1,17 @@
+
+import { SellerNavbar } from "./_components/seller-navbar";
+
+interface DashboardLayoutProps {
+    children: React.ReactNode;
+}
+
+const SellerLayout = ({ children }: DashboardLayoutProps) => {
+    return (
+        <main className="h-full">
+            <SellerNavbar />
+            {children}
+        </main>
+    );
+}
+
+export default SellerLayout;
